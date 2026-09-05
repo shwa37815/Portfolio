87820eb01b1cc7ede20559650154b9b05b763ff4
S.Wada <08shun15@gmail.com>
2026-09-05 11:33:23 +0900

    feat:Homeの実装漏れ

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div style={{ textAlign: 'center', padding: '40px 0', }}> 用意中。
+            <h2>ホーム</h2>
+        </div>
+    );
+}
+
+export default Home;
