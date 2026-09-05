998cdd8f540d81315396f9a359ed74ba4de8b322
S.Wada <08shun15@gmail.com>
2026-09-05 23:08:41 +0900

    memoに複数ページ追加、学習進捗確認可能

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,8 @@
 const Home = () => {
     return (
-        <div style={{ textAlign: 'center', padding: '40px 0', }}> 用意中。
-            <h2>ホーム</h2>
+        <div style={{ textAlign: 'center', padding: '40px 0', }}> 
+        <h2>ホーム</h2>
+            <p style = {{ fontSize: '1.2em', color: '#666' }}>仮のページ。</p>
         </div>
     );
 }
